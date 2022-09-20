ccc55f9170733d99ef67ff70e363a089415518ae
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2022-09-20 23:48:16 +0600

    Update socket.py

--- socket.py
+++ socket.py
@@ -1,4 +1,4 @@
 import socket
 mysocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 mysocket.connect(('data.pr4e.org',80))
-cmd='GET http://data.pri4e'
+cmd='GET http://data.pri4e.org/romeo.txt HTTP/1.0\n\n'.encode()
